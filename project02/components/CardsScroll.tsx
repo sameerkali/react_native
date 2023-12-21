611de6e19c0813922be48bb25ba6544667132470
Sameer Faridi <s.faridi007@gmail.com>
2023-12-21 18:16:55 +0530

    adding a new projext to learn deep linking and cads

---
+++ project02/components/CardsScroll.tsx
@@ -0,0 +1,16 @@
+import { StyleSheet, Text, View, ScrollView } from "react-native";
+import React from "react";
+
+const CardsScroll = () => {
+  return (
+    <View>
+      <ScrollView>
+        <Text>CardsScroll</Text>
+      </ScrollView>
+    </View>
+  );
+};
+
+export default CardsScroll;
+
+const styles = StyleSheet.create({});
