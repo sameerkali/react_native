3ea0a2e971ee6d078c077f47e624882a29cae972
Sameer Faridi <s.faridi007@gmail.com>
2023-12-22 10:22:52 +0530

    make all states and functions

--- project03/App.tsx
+++ project03/App.tsx
@@ -1,16 +1,33 @@
 import { StyleSheet, Text, View } from "react-native";
-import React from "react";
+import React, { useState } from "react";
 import * as Yup from "yup";
 
 const passwordSchema = Yup.object().shape({
   passwordLength: Yup.number()
-  .min(4, "should be at least 4 characters")
-  .min(16, "should be max of 16 characters")
-  .required("length is required")
+    .min(4, "should be at least 4 characters")
+    .min(16, "should be max of 16 characters")
+    .required("length is required")
 });
 
 const App = () => {
-  return <View></View>;
+  //states
+  const [password, setpassword] = useState("");
+  const [isPasswordGenerated, setisPasswordGenerated] = useState(false);
+  const [lowercase, setlowercase] = useState(false);
+  const [uppercase, setuppercase] = useState(false);
+  const [number, setnumber] = useState(false);
+  const [symbol, setsymbol] = useState(false);
+
+  //functions
+  const generatePasswordString = (passwordLength: number) => {}
+  const createPassword = (characters: string ,passwordLength: number) => {}
+  const resetPassword = () => {}
+
+  return (
+    <View>
+      <Text> hii </Text>
+    </View>
+  );
 };
 
 export default App;
