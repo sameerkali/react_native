f6a967042440ab32ab233cd0acf3a4775bdddb4c
Sameer Faridi <s.faridi007@gmail.com>
2023-12-23 21:04:09 +0530

    add header

--- project05/App.tsx
+++ project05/App.tsx
@@ -49,7 +49,7 @@ const App: React.FC = () => {
 
   return (
     <View style={menuStyles.container}>
-      {/* <Header/> */}
+      <Header/>
       <Text style={menuStyles.headerText}>View Menu</Text>
       {/* <FlatList
         data={menuItemsToDisplay}
