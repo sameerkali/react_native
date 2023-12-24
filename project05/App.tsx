d84be561a2bd12296f82fe970403395781a85eb2
Sameer Faridi <s.faridi007@gmail.com>
2023-12-24 12:48:14 +0530

    make a form (input field)

--- project05/App.tsx
+++ project05/App.tsx
@@ -1,5 +1,12 @@
-import React from "react";
-import { View, Text, StyleSheet, FlatList } from "react-native";
+import React, { useState } from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  FlatList,
+  ScrollView,
+  TextInput
+} from "react-native";
 import MenuItems from "./MenuItems";
 import Header from "./Header";
 import Footer from "./Footer";
@@ -48,22 +55,52 @@ const App: React.FC = () => {
     <Item name={item.name} />
   );
 
+  const [firstName, onChangeFirstName] = useState("");
+  const [lastName, onChangeLastName] = useState("");
+  const [message, onChangeMessage] = useState("");
+
   return (
-    <View style={menuStyles.container}>
-      <Header/>
-      <Text style={menuStyles.headerText}>View Menu</Text>
-      {/* <FlatList
-        data={menuItemsToDisplay}
-        keyExtractor={(item) => item.id}
-        renderItem={renderItem}
-      /> */}
-      <MenuItems />
-      <Footer/>
-    </View>
+    // <View style={menuStyles.container}>
+    //   <Header/>
+    //   <Text style={menuStyles.headerText}>View Menu</Text>
+    //   {/* <FlatList
+    //     data={menuItemsToDisplay}
+    //     keyExtractor={(item) => item.id}
+    //     renderItem={renderItem}
+    //   /> */}
+    //   <MenuItems />
+    //   <Footer/>
+    // </View>
+
+    <ScrollView style={styles.container}>
+      <Text style={styles.headingSection}>
+       How was your visit to Little Lemon?
+      </Text>
+      <Text style={styles.infoSection}>
+        Little Lemon is a charming neighborhood bistro that serves
+        simple food and classic cocktails in a lively but casual
+        environment. We would love to hear your experience with us!
+      </Text>
+      <TextInput
+        style={styles.input}
+        value={firstName}
+        onChangeText={onChangeFirstName}
+      />
+      <TextInput
+        style={styles.input}
+        value={lastName}
+        onChangeText={onChangeLastName}
+      />
+      <TextInput
+        style={styles.messageInput}
+        value={message}
+        onChangeText={onChangeMessage}
+      />
+    </ScrollView>
   );
 };
 
-const menuStyles = StyleSheet.create({
+const styles = StyleSheet.create({
   container: {
     flex: 1,
     height: "100%"
@@ -82,6 +119,39 @@ const menuStyles = StyleSheet.create({
   itemText: {
     color: "#F4CE14",
     fontSize: 36
+  },
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+    fontSize: 16,
+    borderColor: "EDEFEE",
+    backgroundColor: "#F4CE14"
+  },
+  messageInput: {
+    height: 100,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+    fontSize: 16,
+    backgroundColor: "#F4CE14"
+  },
+  infoSection: {
+    fontSize: 24,
+    padding: 20,
+    marginVertical: 8,
+    color: "#EDEFEE",
+    textAlign: "center",
+    backgroundColor: "#495E57"
+  },
+  headingSection: {
+    fontSize: 28,
+    padding: 20,
+    marginVertical: 8,
+    color: "#EDEFEE",
+    textAlign: "center",
+    backgroundColor: "#495E57"
   }
 });
 
