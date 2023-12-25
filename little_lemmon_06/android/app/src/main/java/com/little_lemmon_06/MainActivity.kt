2e20a615126add816073d7b20df4afe39ea17c53
Sameer Faridi <s.faridi007@gmail.com>
2023-12-25 12:35:11 +0530

    start making little lemon app

---
+++ little_lemmon_06/android/app/src/main/java/com/little_lemmon_06/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.little_lemmon_06
+
+import com.facebook.react.ReactActivity
+import com.facebook.react.ReactActivityDelegate
+import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
+import com.facebook.react.defaults.DefaultReactActivityDelegate
+
+class MainActivity : ReactActivity() {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  override fun getMainComponentName(): String = "little_lemmon_06"
+
+  /**
+   * Returns the instance of the [ReactActivityDelegate]. We use [DefaultReactActivityDelegate]
+   * which allows you to enable New Architecture with a single boolean flags [fabricEnabled]
+   */
+  override fun createReactActivityDelegate(): ReactActivityDelegate =
+      DefaultReactActivityDelegate(this, mainComponentName, fabricEnabled)
+}
---
+++ little_lemmon_06/src/navigators/RootNavigator.js
@@ -0,0 +1,16 @@
+import * as React from "react";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import WelcomeScreen from "../screens/WelcomeScreen";
+import SubscribeScreen from "../screens/SubscribeScreen";
+
+const Stack = createNativeStackNavigator();
+
+const RootNavigator = () => {
+  return (
+    <Stack.Navigator>
+      {/* Set up stack navigation to move between welcome screen and subscribe screen here */}
+    </Stack.Navigator>
+  );
+};
+
+export default RootNavigator;
