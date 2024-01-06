b5fab2866dceb38aa345f1a45bd74ed0282f6d28
Sameer Faridi <s.faridi007@gmail.com>
2024-01-06 22:33:57 +0530

    errors hai bahot

--- demoPlayer/index.js
+++ demoPlayer/index.js
@@ -1,9 +1,11 @@
 /**
  * @format
  */
-
+import TrackPlayer from 'react-native-track-player';
 import {AppRegistry} from 'react-native';
-import App from './App';
+import App from './src/App';
 import {name as appName} from './app.json';
+import {playbackService} from './musicPlayerServices'
 
 AppRegistry.registerComponent(appName, () => App);
+TrackPlayer.registerPlaybackService(() => playbackService);
