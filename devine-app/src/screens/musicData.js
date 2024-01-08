ba36c7e869c64fe3713d295fe950d1f1ea45c7a5
Sameer Faridi <s.faridi007@gmail.com>
2024-01-08 16:34:30 +0530

    remove some changes

--- devine-app/src/screens/musicData.js
+++ devine-app/src/screens/musicData.js
@@ -1,149 +1,27 @@
-<<<<<<< HEAD
-// export const playerDeeksha = [
-//   {
-//   id: '1',
-//   url: require('../../assets/audio/om.mp3'),
-//   title: 'new',
-//   artist: 'descripstion',
-//   artwork: require('../../assets/img/img2.jpg'),
-//   duration: 60,
-// },
-//     {
-//       id: '2',
-//       url: require('../../assets/audio/om.mp3'),
-//       title: 'Modern Chillout',
-//       artist: 'penguinmusic',
-//       artwork: require('../../assets/img/img2.jpg'),
-//       duration: 66,
-//     },
-//     {
-//       id: '3',
-//       url: require('../../assets/audio/josh.mp3'),
-//       title: 'Powerful Beat',
-//       artist: 'penguinmusic',
-//       artwork: require('../../assets/img/img3.jpg'),
-//       duration: 73,
-//     }
-//   ]
 
-
-
-// // // YourComponent.js
-// // import React, { useEffect } from 'react';
-// // import { View, Text, Button } from 'react-native';
-// // import { useDispatch, useSelector } from 'react-redux';
-// // import { setData } from '../redux/action';
-// // import { BaseUrl } from '../BaseUrl';
-
-
-// // const musicData = () => {
-// //   const dispatch = useDispatch();
-// //   const { data, error } = useSelector((state) => state);
-// //   console.log(data ,'datanjnjmf')
-
-// //   const fetchData = async () => {
-// //     try {
-// //       const response = await fetch(`${BaseUrl}/getAllMantras`);
-// //       console.log(response,'responsenjnfkjnfkjnfkj');
-// //       const result = await response.json();
-// //       console.log(result ,'results')
-// //       // dispatch(setData(result));
-// //     } catch (err) {
-// //       // dispatch(setError(err.message));
-// //     }
-// //   };
-
-// //   useEffect(() => {
-// //     fetchData();
-// //   }, []);
-
-// //   return (
-// //     <View>
-// //       <Text>{error ? `Error: ${error}` : 'Data fetched successfully!'}</Text>
-// //       {data && (
-// //         <View>
-// //           <Text>Data:</Text>
-// //           <Text>{JSON.stringify(data)}</Text>
-// //         </View>
-// //       )}
-// //       <Button title="Fetch Data" onPress={fetchData} />
-// //     </View>
-// //   );
-// // };
-
-// // export default musicData;
-=======
 export const playerDeeksha = [
   {
-  id: '1',
-  url: require('../../assets/audio/om.mp3'),
-  title: 'new',
-  artist: 'descripstion',
-  artwork: require('../../assets/img/img2.jpg'),
-  duration: 60,
-},
-    {
-      id: '2',
-      url: require('../../assets/audio/om.mp3'),
-      title: 'Modern Chillout',
-      artist: 'penguinmusic',
-      artwork: require('../../assets/img/img2.jpg'),
-      duration: 66,
-    },
-    {
-      id: '3',
-      url: require('../../assets/audio/josh.mp3'),
-      title: 'Powerful Beat',
-      artist: 'penguinmusic',
-      artwork: require('../../assets/img/img3.jpg'),
-      duration: 73,
-    }
-  ]
-
-
-
-// // YourComponent.js
-// import React, { useEffect } from 'react';
-// import { View, Text, Button } from 'react-native';
-// import { useDispatch, useSelector } from 'react-redux';
-// import { setData } from '../redux/action';
-// import { BaseUrl } from '../BaseUrl';
-
-
-// const musicData = () => {
-//   const dispatch = useDispatch();
-//   const { data, error } = useSelector((state) => state);
-//   console.log(data ,'datanjnjmf')
-
-//   const fetchData = async () => {
-//     try {
-//       const response = await fetch(`${BaseUrl}/getAllMantras`);
-//       console.log(response,'responsenjnfkjnfkjnfkj');
-//       const result = await response.json();
-//       console.log(result ,'results')
-//       // dispatch(setData(result));
-//     } catch (err) {
-//       // dispatch(setError(err.message));
-//     }
-//   };
-
-//   useEffect(() => {
-//     fetchData();
-//   }, []);
-
-//   return (
-//     <View>
-//       <Text>{error ? `Error: ${error}` : 'Data fetched successfully!'}</Text>
-//       {data && (
-//         <View>
-//           <Text>Data:</Text>
-//           <Text>{JSON.stringify(data)}</Text>
-//         </View>
-//       )}
-//       <Button title="Fetch Data" onPress={fetchData} />
-//     </View>
-//   );
-// };
-
-// export default musicData;
->>>>>>> b25350939607eec99add5ed69918b1fe548498a1
+    id: '1',
+    url: require('../../assets/audio/om.mp3'),
+    title: 'new',
+    artist: 'descripstion',
+    artwork: require('../../assets/img/img2.jpg'),
+    duration: 60,
+  },
+  {
+    id: '2',
+    url: require('../../assets/audio/om.mp3'),
+    title: 'Modern Chillout',
+    artist: 'penguinmusic',
+    artwork: require('../../assets/img/img2.jpg'),
+    duration: 66,
+  },
+  {
+    id: '3',
+    url: require('../../assets/audio/josh.mp3'),
+    title: 'Powerful Beat',
+    artist: 'penguinmusic',
+    artwork: require('../../assets/img/img3.jpg'),
+    duration: 73,
+  }
+]
